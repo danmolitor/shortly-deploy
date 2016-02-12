4febf4d61399198e67bfe225d56cc6994aff4ddc
danmolitor <danmolitor91@gmail.com>
2016-02-11 18:06:47 -0800

    refactored for mongodb

--- app/models/user.js
+++ app/models/user.js
@@ -5,10 +5,20 @@ var mongoose = require('mongoose');
 
 //Schema creation
 var userSchema = mongoose.Schema({
-  username: String,
-  password: String,
-  ts: String
+  username: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
+
+User = mongoose.model('User', userSchema);
 
 userSchema.pre('save', function(next){
   var cipher = Promise.promisify(bcrypt.hash);
@@ -19,67 +29,16 @@ userSchema.pre('save', function(next){
     });
 });
 
-userSchema.methods.comparePassword = function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
-    callback(isMatch);
+userSchema.methods.comparePassword = function(candidatePassword, savedPassword, callback) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    console.log(err);
+    console.log(isMatch);
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
   });
 }
 
 //Creating the model
-module.exports.User = User = db.model('User', userSchema);
-
-//Instantiating
-// var gordon = new User({username: "Gordon", password: "password"})
-
-// gordon.save(function(err, instance) {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log(instance);
-//   instance.comparePassword('password', function(isMatch) {
-//     console.log('match: ', isMatch);
-//   })
-// })
-
-// User.find(function (err, instance) {
-//   if (err) return console.error(err);
-//   console.log(instance);
-// })
-
-// User.find({username: 'Gordon'}, function(err, instance) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(instance);
-// })
-
-
-
-
-
-
-// var db = require('../config');
-// var bcrypt = require('bcrypt-nodejs');
-// var Promise = require('bluebird');
-
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
-
-// module.exports = User;
+module.exports = User;
